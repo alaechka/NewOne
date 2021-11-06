2f7047e8b1050c4b739a17a70a3ea8c27f13e00d
alina alaeva <alinaalaeva@MacBook-Air-alina.local>
2021-11-06 09:11:50 -0700

    First homework2

---
+++ src/homework/homework1.java
@@ -0,0 +1,44 @@
+package homework;
+
+public class homework1 {
+    public static void main(String[] args) {
+        float mortageRate =2.4f;
+        System.out.println("Mortagage Rate ="+ mortageRate );
+
+        // Number of hours in day
+        byte numberOfHourse=24;
+        System.out.println("\nNumber of hours in a day ="+numberOfHourse);
+
+        // max numbers of days in year
+
+        short numOfDays=365;
+        System.out.println("\nMax number of days in year ="+numOfDays);
+
+        // Total number of Employees in Amazon
+
+        int numOfEmpl=1335000;
+        System.out.println("\nTotal number employees in Amazon="+numOfEmpl);
+
+        // total population of united States
+
+        int numOfPopulation=331449281;
+        System.out.println("\nTotal population of The USA="+numOfPopulation);
+
+        //Balance in a bank account
+
+        double balanceInAccount=5.765876;
+        System.out.println("\nBalance in Bank account ="+balanceInAccount);
+
+        //Does the Sun rise from the West?
+
+        boolean sunRiseFromWest=false;
+        System.out.println("\nThe Sun rise from the West? ="+sunRiseFromWest);
+
+        // Initials for your name
+
+        char firstN='A';
+        char SecN='A';
+        System.out.println("\nMy initials ="+firstN+SecN);
+
+    }
+}
